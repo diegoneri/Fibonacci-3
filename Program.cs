90179838f25bc69f67af3d77baead280624f272a
HenryKasai <henrykasai1775@gmail.com>
2020-07-03 20:08:25 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool terminar = false;
+            int N;
+            int x = 0;
+            int y = 1;
+            while (!terminar)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("\nQuantos termos da sequência de Fibonacci você deseja ver? ");
+                bool nbool = Int32.TryParse(Console.ReadLine(), out N);
+                if (!nbool)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Valor inválido, pressione uma tecla para inserir outro valor.");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    int menos = N - 2;
+                    Console.Write($"{x} {y} ");
+                    for (int i = 1; i <= menos; i++)
+                    {
+                        int result = x + y;
+                        Console.Write($"{result} "); 
+                        x = y;
+                        y = result;
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine("\nPressione uma tecla para finalizar o programa.");
+                    Console.ReadKey();
+                    terminar = true;
+                }
+            }
+        }
+    }
+}
